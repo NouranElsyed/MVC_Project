fe943e751b6dc01a9b56993abca797a7f5151f1b
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-09-12 19:59:49 +0300

    first commit

---
+++ Demo.DAL/Context/MvcDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Context
+{
+    public class MvcDbContext : DbContext
+    {
+        public MvcDbContext(DbContextOptions<MvcDbContext> options):base(options) { }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=.; Database=MvcDb;Trusted-connection=true;trustedServerCertificate=ture");
+          
+        //}
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,96 @@
+﻿using Demo.BLL.Interfaces;
+using Demo.BLL.Repositories;
+using Demo.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace Demo.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository) 
+        {
+            _departmentRepository = departmentRepository;
+
+        }
+        public IActionResult Index()
+        {
+            var department = _departmentRepository.GetAll();
+            return View(department);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid) 
+            {
+                _departmentRepository.Add(department);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+        public IActionResult Details(int? id,string ViewName = "Details") 
+        {
+            if (id is null) { return BadRequest(); }
+
+            var department = _departmentRepository.GetById(id.Value);
+            if (department is null) { return NotFound(); }
+            return View(ViewName,department);
+
+        }
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+            if (id is null) { return BadRequest(); }
+
+            var department = _departmentRepository.GetById(id.Value);
+            if (department is null) { return NotFound(); }
+            return View(department);
+        }
+        [HttpPost]
+        public IActionResult Update(Department department, [FromRoute] int id)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _departmentRepository.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch(System.Exception ex)
+                { 
+                ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+            return View(department);
+        }
+      
+        public IActionResult Delete(int? id) 
+        {
+            return Details(id,"Delete");
+        }
+        [HttpPost]
+        public IActionResult Delete(Department department, [FromRoute] int id) 
+        {
+                if ((id != department.Id))
+                { return BadRequest(); }
+                try
+                {
+                    _departmentRepository.Delete(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (System.Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            return View(department);
+        }
+
+    }
+}
